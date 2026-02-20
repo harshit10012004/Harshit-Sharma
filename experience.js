8e9ac1662d3d402dacc898e7c6d6432da1523204
harshit10012004 <harshitynr8@gmail.com>
2026-02-20 18:25:40 +0530

    Downgrade animejs to v3.2.1 and fix import

--- experience.js
+++ experience.js
@@ -1,4 +1,4 @@
-import anime from 'animejs';
+import anime from 'animejs/lib/anime.es.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   anime({
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import anime from 'animejs';
+import anime from 'animejs/lib/anime.es.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const stats = {
